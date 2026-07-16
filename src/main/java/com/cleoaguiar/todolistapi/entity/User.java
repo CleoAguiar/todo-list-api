e7650654e00915ea557e57476fd83544bd18764a
Cleo Aguiar <cleotaisas@gmail.com>
2026-07-15 22:07:59 -0300

    feat(auth): add User entity and repository

--- src/main/java/com/cleoaguiar/todolistapi/entity/User.java
+++ src/main/java/com/cleoaguiar/todolistapi/entity/User.java
@@ -1,16 +1,20 @@
 package com.cleoaguiar.todolistapi.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "O nome do usuário é obrigatório.")
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @NotBlank(message = "A senha é obrigatória.")
+    @Column(nullable = false)
     private String password;
 }
 
