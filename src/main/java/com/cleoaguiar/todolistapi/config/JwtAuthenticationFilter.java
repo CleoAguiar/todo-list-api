bff6f972341a3ea922909ae66c1ada605f849fbb
Cleo Aguiar <cleotaisas@gmail.com>
2026-07-19 18:31:40 -0300

    refactor: prepare jwt service for Spring Security integration

---
+++ src/main/java/com/cleoaguiar/todolistapi/config/JwtAuthenticationFilter.java
@@ -0,0 +1,70 @@
+package com.cleoaguiar.todolistapi.config;
+
+import com.cleoaguiar.todolistapi.entity.User;
+import com.cleoaguiar.todolistapi.repository.UserRepository;
+import com.cleoaguiar.todolistapi.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public JwtAuthenticationFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+        throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwtToken = authHeader.substring(7);
+        String username = jwtService.extractUsername(jwtToken);
+
+        Optional<User> userOptional = userRepository.findByEmail(username);
+
+        if(userOptional.isEmpty()) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        User user = userOptional.get();
+
+        if(!jwtService.isTokenValid(jwtToken, user.getEmail())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                user,
+                null,
+                null
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/cleoaguiar/todolistapi/service/JwtService.java
+++ src/main/java/com/cleoaguiar/todolistapi/service/JwtService.java
@@ -1,5 +1,6 @@
 package com.cleoaguiar.todolistapi.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +19,18 @@ public class JwtService {
         return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
     public String generateToken(String username) {
         return Jwts.builder()
                 .subject(username)
@@ -26,4 +39,18 @@ public class JwtService {
                 .signWith(getSignInKey())
                 .compact();
     }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public Date extractExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+
+    public boolean isTokenValid(String token, String username) {
+        String extractedUsername = extractUsername(token);
+        return extractedUsername.equals(username) && !isTokenExpired(token);
+    }
+
 }
