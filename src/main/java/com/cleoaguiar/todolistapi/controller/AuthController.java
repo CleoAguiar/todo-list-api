3b1ec0a85677dbc63b0cb84a7ad049cc6810ec9d
Cleo Aguiar <cleotaisas@gmail.com>
2026-08-02 00:40:32 -0300

    feat: add OpenAPI documentation and Swagger UI (#13)
    * feat: add SpringDoc OpenAPI and expose Swagger endpoints
    
    * doc: add OpenAPI documentation for auth and todo endpoints
    
    * refact(docs): improve OpenAPI response schemas

--- src/main/java/com/cleoaguiar/todolistapi/controller/AuthController.java
+++ src/main/java/com/cleoaguiar/todolistapi/controller/AuthController.java
@@ -4,7 +4,14 @@ import com.cleoaguiar.todolistapi.dto.AuthRequest;
 import com.cleoaguiar.todolistapi.dto.AuthResponse;
 import com.cleoaguiar.todolistapi.dto.UserRegisterRequest;
 import com.cleoaguiar.todolistapi.dto.UserResponse;
+import com.cleoaguiar.todolistapi.exception.ErrorResponse;
 import com.cleoaguiar.todolistapi.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Authentication", description = "Endpoint de autenticação e registro de usuários")
 public class AuthController {
     private final AuthService authService;
 
@@ -23,12 +31,42 @@ public class AuthController {
     }
 
     @PostMapping("/register")
+    @Operation(
+            summary = "Registrar usuário",
+            description = "Cria uma conta de usuário"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    ))
+
+    })
     public ResponseEntity<UserResponse> register(@Valid @RequestBody UserRegisterRequest request) {
         authService.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/login")
+    @Operation(
+            summary = "Realizar login",
+            description = "Autentica o usuário e retorna um token JWT"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login realizado com sucesso",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AuthResponse.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "400", description = "Credencias inválidas",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    ))
+    })
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthRequest authRequest) {
         String token = authService.login(authRequest);
         return ResponseEntity.ok(new AuthResponse(token));
