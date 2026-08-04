9085cb8d7602502b5f1b518e1fcc5f4046601017
Cleo Aguiar <cleotaisas@gmail.com>
2026-08-04 20:11:51 -0300

    test: add successful authentication unit test

--- src/test/java/com/cleoaguiar/todolistapi/service/AuthServiceTest.java
+++ src/test/java/com/cleoaguiar/todolistapi/service/AuthServiceTest.java
@@ -1,5 +1,7 @@
 package com.cleoaguiar.todolistapi.service;
 
+import com.cleoaguiar.todolistapi.dto.AuthRequest;
+import com.cleoaguiar.todolistapi.dto.AuthResponse;
 import com.cleoaguiar.todolistapi.dto.UserRegisterRequest;
 import com.cleoaguiar.todolistapi.entity.User;
 import com.cleoaguiar.todolistapi.repository.UserRepository;
@@ -9,6 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -77,5 +81,29 @@ public class AuthServiceTest {
         verify(userRepository).existsByEmail("cleo@email.com");
         verify(passwordEncoder, never()).encode((anyString()));
         verify(userRepository, never()).save(any(User.class));
+    }
+
+    @Test
+    void shouldAuthenticateUserSuccessfully() {
+        AuthRequest request = new AuthRequest("cleo@email.com", "123456");
+
+        User user = new User();
+        user.setUsername("cleo");
+        user.setEmail("cleo@email.com");
+        user.setPassword("senhaCriptografada");
+
+        when(userRepository.findByEmail("cleo@email.com")).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches("123456", "senhaCriptografada"))
+                .thenReturn(true);
+        when(jwtService.generateToken("cleo@email.com")).thenReturn("jwt-token");
+
+        String token = authService.login(request);
+
+        assertNotNull(token);
+        assertEquals("jwt-token", token);
+
+        verify(userRepository).findByEmail("cleo@email.com");
+        verify(passwordEncoder).matches("123456", "senhaCriptografada");
+        verify(jwtService).generateToken("cleo@email.com");
     }
 }
